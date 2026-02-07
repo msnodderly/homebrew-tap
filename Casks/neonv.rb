b4482778fdd3e9fb7941982d3a7b3d2eecbfb16d
Matt S. <mds@area51a.net>
2026-02-06 23:19:10 -0800

    chore: Update neonv to v0.11.0

--- Casks/neonv.rb
+++ Casks/neonv.rb
@@ -1,6 +1,6 @@
 cask "neonv" do
-  version "0.10.2"
-  sha256 "ba855481f365e0e2b467d21df678cf8f22d30b8cf02d4e854c6342b50ab4a1a2"
+  version "0.11.0"
+  sha256 "4bbea534e6482287889e610d8f997d2d24e304d9eac380bde422f79bb9cfab1d"
 
   url "https://github.com/msnodderly/neonv/releases/download/v#{version}/NeoNV-#{version}-macos-universal.dmg"
   name "NeoNV"
